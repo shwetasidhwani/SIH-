61bad440d78dd4a9bb0fb0ba4640782810da0b6d
AshmitSrivastava <ashmitsrivastava1607@gmail.com>
2024-08-24 20:45:25 +0530

    added backend stucture

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,11 +1,50 @@
+//Imports 
 const express = require("express");
-const { AssemblyAI } = require("assemblyai");
 const cors = require("cors");
+const { AssemblyAI } = require("assemblyai");
+const body_parser = require('body-parser');
+const cookie_parser = require("cookie-parser");
+const mongoose = require('mongoose');
+const session = require('express-session');
 
+//Ash imports 
+
+
+
+
+
+
+
+//Shwets imports
+
+
+
+
+
+
+
+//Josh imports 
+
+
+
+
+
+
+
+
+
+//config imports
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+
+//Configuration
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended : true}));
 app.use(express.json());
 
 const client = new AssemblyAI({
@@ -27,6 +66,26 @@ app.post("/transcribe", async (req, res) => {
   }
 });
 
+
+
+//database
+
+
+
+
+
+
+
+
+//-----------------------Routes-------------------------------
+
+
+
+
+
+
+
+//start
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
