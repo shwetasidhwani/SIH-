cc2ae95037c6d3a36a174f9d289c2d63cb6efaa9
shwetasidhwani <shweta.sidhwani@gmail.com>
2024-08-26 16:24:48 +0530

    navbar and hero

--- FRONTEND/src/components/HeroComponent.jsx
+++ FRONTEND/src/components/HeroComponent.jsx
@@ -6,8 +6,14 @@ const HeroComponent = () => {
   return (
     <div className="hero">
       <div className="hero-content">
-        <h1>StationSathi</h1>
-        <p>Find the best train routes and book tickets online</p>
+        <h1 className="hero-catchphrase">
+          A Railway <span className="underlined-hero">Navigation</span> System{" "}
+        </h1>
+        <p className="hero-supportphrase">
+          Effortlessly navigate your journeys with StationSathi. <br />
+          Discover real-time guidance and simplify your travel with convenient
+          insights into stations and routes.
+        </p>
       </div>
     </div>
   );
--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -1,31 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/global.css";
+
 const NavBar = () => {
   return (
     <div className="homepage-navbar">
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About Us</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-
-      <ul className="auth-links">
-        <li className="login">
-          <a href="#">Log In</a>
-        </li>
-        <li className="signup">
-          <a href="#">Sign Up</a>
-        </li>
-      </ul>
+      <div className="navbar-left">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-center">
+        <a href="#" className="website-name">
+          StationSathi
+        </a>
+      </div>
+      <div className="navbar-right">
+        <ul className="auth-links">
+          <li className="login">
+            <a href="#">Log In</a>
+          </li>
+          <li className="signup">
+            <a href="#">Sign Up</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
