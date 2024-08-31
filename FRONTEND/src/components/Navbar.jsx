fc59e26f507c5a8c64831dc16f3e089c1116dcf3
Dhruvt0310 <taterdhruv0310@gmail.com>
2024-08-31 11:44:52 +0530

    fixed navbar issue

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -1,18 +1,38 @@
-  import React, { useState, useEffect } from "react";
-  import "../styles/global.css";
-  import { Link } from "react-router-dom";
-  import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "../styles/global.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const NavBar = () => {
+const Navbar = () => {
+  const [user, setUser] = useState(null); // Declare user state
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/auth/profile", {
+          withCredentials: true,
+        });
+        console.log(response.data);
+        setUser(response.data); // Set user data
+      } catch (err) {
+        console.error("Error fetching user profile:", err);
+      }
+    };
+    fetchUserProfile();
+  }, []);
+
   return (
     <div className="homepage-navbar">
       <div className="navbar-left">
         <ul>
           <li>
-            <Link to = "/">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
           </li>
           <li>
             <a href="#">Contact</a>
@@ -25,79 +45,28 @@ const NavBar = () => {
         </a>
       </div>
       <div className="navbar-right">
-        <ul className="auth-links">
-          <li className="login">
-            <Link to = "/login">Log In</Link>
-          </li>
-          <li className="signup">
-            <Link to = "/signup">Sign Up</Link>
-          </li>
-        </ul>
+        {user ? (
+          <div className="user-profile">
+            <img
+              src={`http://localhost:3000${user.profilePicture}`}
+              alt="Profile"
+              className="profile-picture"
+            />
+            <span>{user.email}</span>
+          </div>
+        ) : (
+          <ul className="auth-links">
+            <li className="login">
+              <Link to="/login">Log In</Link>
+            </li>
+            <li className="signup">
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
 };
 
-    useEffect(() => {
-      const fetchUserProfile = async () => {
-        try {
-          const response = await axios.get("http://localhost:3000/api/auth/profile", {
-            withCredentials: true,
-          });
-          console.log(response.data);
-          setUser(response.data);
-        } catch (err) {
-          console.error("Error fetching user profile:", err);
-        }
-      };
-      fetchUserProfile();
-    }, []);
-
-    return (
-      <div className="homepage-navbar">
-        <div className="navbar-left">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-center">
-          <a href="#" className="website-name">
-            StationSathi
-          </a>
-        </div>
-        <div className="navbar-right">
-          {user ? (
-            <div className="user-profile">
-              <img
-                src={`http://localhost:3000${user.profilePicture}`}
-                alt="Profile"
-                className="profile-picture"
-              />
-              <span>{user.email}</span>
-            </div>
-          ) : (
-            <ul className="auth-links">
-              <li className="login">
-                <Link to="/login">Log In</Link>
-              </li>
-              <li className="signup">
-                <Link to="/signup">Sign Up</Link>
-              </li>
-            </ul>
-          )}
-        </div>
-      </div>
-    );
-
-  export default NavBar;
+export default Navbar;
