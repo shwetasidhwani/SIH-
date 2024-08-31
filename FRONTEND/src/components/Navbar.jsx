1066b034d4826f039997574bf5c9ce8e926a04ed
AshmitSrivastava <ashmitsrivastava1607@gmail.com>
2024-08-31 14:56:29 +0530

    getting up to date

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -4,39 +4,8 @@
   import axios from "axios";
 
 const NavBar = () => {
-  return (
-    <div className="homepage-navbar">
-      <div className="navbar-left">
-        <ul>
-          <li>
-            <Link to = "/">Home</Link>
-          </li>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-center">
-        <a href="#" className="website-name">
-          StationSathi
-        </a>
-      </div>
-      <div className="navbar-right">
-        <ul className="auth-links">
-          <li className="login">
-            <Link to = "/login">Log In</Link>
-          </li>
-          <li className="signup">
-            <Link to = "/signup">Sign Up</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+
+  const [user , setUser ] = useState(null);
 
     useEffect(() => {
       const fetchUserProfile = async () => {
@@ -99,5 +68,5 @@ const NavBar = () => {
         </div>
       </div>
     );
-
+  };
   export default NavBar;
